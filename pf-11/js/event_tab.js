57540a9c4a8dd367d93f4b74686b28d6d46d001c
notany07 <87401364+notany07@users.noreply.github.com>
2021-08-02 18:02:21 +0900

    Delete pf-11 directory

--- pf-11/js/event_tab.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $(".touchTab ul li").on("click",function(){
-        $(this).addClass('on').siblings().removeClass('on');
-        var x = $(this).index();
-        
-        $('.list').eq(x).removeClass('off').siblings().addClass('off');
-    })
-})
