1fba9a2aa86c9f487c4752fd3247214785e231e8
notany07 <87401364+notany07@users.noreply.github.com>
2021-08-20 14:17:56 +0900

    Delete katri directory

--- katri/js/search.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $(".seach_click").on("click",function(){
-        $(".search").show();
-    });
-
-    $(".close_bar").on("click",function(){
-        $(".search").hide();
-    });
-})
